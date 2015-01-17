a3bc99e5800183dd57c0dd589f93bae476e0036f
Mark Wales <mark@smallhadroncollider.com>
2015-01-17 11:35:56 +0000

    added base plugin code

---
+++ how-secure-is-my-password/how-secure-is-my-password.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Plugin Name: How Secure Is My Password?
+ * Plugin URI: http://github.com/howsecureisymypassword/wordpress
+ * Description: Adds the How Secure Is My Password strength meter to your user profile pages
+ * Version: 0.1
+ * Author: Mark Wales
+ * Author URI: http://smallhadroncollider.com
+ * License: MIT
+ */
+
+    echo get_current_screen();
+function hsimp_scripts() {
+    wp_enqueue_script('hsimp.jquery.min.js', plugins_url('hsimp.jquery.min.js', __FILE__), array('jquery'), '0.1', true);
+    wp_enqueue_style('hsimp.min.css', plugins_url('hsimp.min.css', __FILE__));
+}
+
+add_action('admin_init', 'hsimp_scripts');
